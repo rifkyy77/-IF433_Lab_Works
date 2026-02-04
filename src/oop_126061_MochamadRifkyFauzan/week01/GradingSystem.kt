690ebf020d4b97a3af4cf89be49e9fbe78e22496
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-04 21:48:54 +0700

    week01: initial setup and procedural code

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_126061_MochamadRifkyFauzan.week01
+
+fun main() {
+    var name: String = "jhon Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
