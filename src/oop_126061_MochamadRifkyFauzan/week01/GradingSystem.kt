04339a89b1f1e6965da3520c438eee62ce0664d9
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-04 21:53:38 +0700

    week01: refactor to use val and string templates

--- src/oop_126061_MochamadRifkyFauzan/week01/GradingSystem.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_126061_MochamadRifkyFauzan.week01
 
 fun main() {
-    var name: String = "jhon Thor"
-    var score: Int = 80
+    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "jhon Thor"
+    val score = 80
 
-    // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    // REFACTOR: String Template ($name)
+    println("Nama: $name , Nilai: $score")
 }
