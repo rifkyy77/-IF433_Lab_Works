577b004d20be413cab65b2e47ed48b4c2ade80f9
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-04 22:11:31 +0700

    week01: add calculateStatus function

--- src/oop_126061_MochamadRifkyFauzan/week01/GradingSystem.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/GradingSystem.kt
@@ -16,4 +16,10 @@ fun main() {
         else -> "D"
     }
     println("Grade kamu: $grade")
+
+    // panggil di dalam main()
+    println("Status: ${calculateStatus(score)}")
 }
+
+//Tulis DI LUAR main()
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
