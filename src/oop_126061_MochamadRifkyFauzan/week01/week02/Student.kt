77901b5495c0f0c8436c50c1bfa0eac239b89a5c
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-11 15:53:50 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_126061_MochamadRifkyFauzan/week01/week02/Student.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_126061_MochamadRifkyFauzan.week01.week02
 
-class Student ( val name: String, val nim: String, val major: String, ){
+class Student ( val name: String, val nim: String, val gpa: Double = 0.0, val major: String, ){
     init {
         // validasi sederhana: cek panjang NIM
         if (nim.length !=5) {
