52098adafa48235769d69dffba706b8857ea1606
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-18 15:26:57 +0700

    week03: cleanup code

--- src/oop_126061_MochamadRifkyFauzan/week01/week03/Main.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week03/Main.kt
@@ -3,15 +3,11 @@ package oop_126061_MochamadRifkyFauzan.week01.week03
 fun main() {
     val e = Employee("Budi")
 
-    //1. test validasi salary
     e.salary = -1000// harusnya print error
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    //2. test encapsulation
     e.increasePerformance()
-    // 6. performancing
 
-    // 3. test computed property
     println("pajak yang harus dibayar: ${e.tax}")
 }
