854abaf3aee7febcdecc4957603ce47c6484c666
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-18 15:26:14 +0700

    week03: add private property

--- src/oop_126061_MochamadRifkyFauzan/week01/week03/Main.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week03/Main.kt
@@ -2,6 +2,16 @@ package oop_126061_MochamadRifkyFauzan.week01.week03
 
 fun main() {
     val e = Employee("Budi")
-    println("---Test Error")
-    e.salary = 5000
+
+    //1. test validasi salary
+    e.salary = -1000// harusnya print error
+    e.salary = 5000000
+    println("Gaji: ${e.salary}")
+
+    //2. test encapsulation
+    e.increasePerformance()
+    // 6. performancing
+
+    // 3. test computed property
+    println("pajak yang harus dibayar: ${e.tax}")
 }
