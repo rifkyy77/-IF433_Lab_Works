9460139b44c67d82c0502ebdab6b5bd7701175f7
rifkyy77 <rifkyfauzan82@gmail.com>
2026-02-25 15:26:20 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_126061_MochamadRifkyFauzan/week01/week04/Main.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week04/Main.kt
@@ -21,4 +21,23 @@ fun main() {
     myElectricCar.accelerate()
     myElectricCar.honk()
     myElectricCar.openTrunk()
+
+    println("\n--- Testing Employee Hierarchy ---")
+
+    val manager = Manager(
+        name = "Budi",
+        baseSalary = 8000000
+    )
+
+    val developer = Developer(
+        name = "Andi",
+        baseSalary = 7000000,
+        programmingLanguage = "Kotlin"
+    )
+
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
 }
