2b7bc4de7f9c7c15718bbb30083f5784391bf86f
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-04 15:16:47 +0700

    week05: implement polymorphic collection and smart casting

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week05/Main.kt
@@ -0,0 +1,26 @@
+package oop_126061_MochamadRifkyFauzan.week01.week05
+
+fun main() {
+    val dosen1 = Dosen("pak alex", "0123456")
+    val admin1 = Admin( "bu siti")
+
+    val daftarPegawa: List<Pegawai> = listOf(dosen1, admin1)
+
+    println("=== AKTIVITAS PEGAWAI ===")
+    for (pegawai in daftarPegawa) {
+        // pemanggilan runtime polymorphism
+        pegawai.bekerja()
+
+        when (pegawai) {
+            is Dosen -> {
+                println("=> Terdeteksi sebagai dosen (NIDN: ${pegawai.nidn})")
+                pegawai.mengejar()
+            }
+            is Admin -> {
+                println("=> Terdeteksi sebgai Admin")
+                pegawai.doAdminWork()
+            }
+        }
+        println("---------------------")
+    }
+}
