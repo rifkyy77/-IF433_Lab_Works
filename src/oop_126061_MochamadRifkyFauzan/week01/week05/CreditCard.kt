88081daf5989dd61f5073288c7dcf46c810b2075
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-04 15:27:39 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week05/CreditCard.kt
@@ -0,0 +1,15 @@
+package oop_126061_MochamadRifkyFauzan.week01.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Pembayaran sebesar $amount berhasil. Total terpakai: $usedAmount")
+        } else {
+            println("Transaksi ditolak")
+        }
+    }
+}
