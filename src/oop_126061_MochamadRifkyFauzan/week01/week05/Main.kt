09e90acb3364d608e1735806ed54f39fffb79e82
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-04 15:22:30 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_126061_MochamadRifkyFauzan/week01/week05/Main.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week05/Main.kt
@@ -2,13 +2,12 @@ package oop_126061_MochamadRifkyFauzan.week01.week05
 
 fun main() {
     val dosen1 = Dosen("pak alex", "0123456")
-    val admin1 = Admin( "bu siti")
+    val admin1 = Admin("bu siti")
 
     val daftarPegawa: List<Pegawai> = listOf(dosen1, admin1)
 
     println("=== AKTIVITAS PEGAWAI ===")
     for (pegawai in daftarPegawa) {
-        // pemanggilan runtime polymorphism
         pegawai.bekerja()
 
         when (pegawai) {
@@ -23,4 +22,11 @@ fun main() {
         }
         println("---------------------")
     }
+
+    val math = MathHelper()
+
+    println("=== COMPILE TIME POLYMORPHISM ===")
+    println("Luas Persegi: ${math.hitungLuas(4)}")
+    println("Luas Persegi Panjang: ${math.hitungLuas(5, 3)}")
+    println("Luas Lingkaran: ${math.hitungLuas(7.0)}")
 }
