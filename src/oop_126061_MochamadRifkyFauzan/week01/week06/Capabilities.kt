b844f2a00985fd7f1d3aa02741af507e184d5145
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-11 15:06:11 +0700

    week06: create compound interfaces

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week06/Capabilities.kt
@@ -0,0 +1,10 @@
+package oop_126061_MochamadRifkyFauzan.week01.week06
+
+// Interface = Can-Do (Behaavior)
+interface BluetoothConnectable {
+    fun connectToBluetooth()
+}
+
+interface Rechargeable{
+    fun chargeBattery()
+}
