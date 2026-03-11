636c3cb24f17146d8ebc4e662bc699d819a78e4b
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-11 15:39:05 +0700

    week06: (task) create SmartHomeHub logic

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week06/SmartHomeHub.kt
@@ -0,0 +1,19 @@
+package oop_126061_MochamadRifkyFauzan.week01.week06
+
+class SmartHomeHub {
+
+    val devices = mutableListOf<SmartDevice>()
+
+    fun addDevice(device: SmartDevice) {
+        devices.add(device)
+        println("${device.name} berhasil ditambahkan ke Smart Home.")
+    }
+
+    fun turnOffAllSwitches() {
+        for (device in devices) {
+            if (device is Switchable) {
+                device.turnOff()
+            }
+        }
+    }
+}
