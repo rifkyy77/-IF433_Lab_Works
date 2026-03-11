70cfaad5738cbc31b4dc5e38feedf2ceb94545e5
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-11 15:14:50 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_126061_MochamadRifkyFauzan.week01.week06
+
+interface Camera {
+    fun turnOn() {println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() {println("Sinyal seluler mecari jaringan.") }
+}
