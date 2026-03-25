fbc6d546d1a0bb74ff6382f592de5c7ed8b73ba0
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-25 15:49:25 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week07/GameManager.kt
@@ -0,0 +1,15 @@
+package oop_126061_MochamadRifkyFauzan.week01.week07
+
+object GameManager {
+
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
