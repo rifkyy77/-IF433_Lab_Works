a813b50d9ac97818a4a7b44e905a890efdd6f521
rifkyy77 <rifkyfauzan82@gmail.com>
2026-03-25 15:50:30 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_126061_MochamadRifkyFauzan.week01.week07
+
+enum class ItemRarity(val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(20),
+    RARE(7),
+    EPIC(2),
+    LEGENDARY(1)
+}
