d810ff7a5b4fe8ea4105e4ae2b4c4fbe3e766cdd
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-15 15:33:57 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

--- src/oop_126061_MochamadRifkyFauzan/week01/week08/EcommerceModels.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week08/EcommerceModels.kt
@@ -1,2 +1,15 @@
 package oop_126061_MochamadRifkyFauzan.week01.week08
 
+sealed class Product
+
+data class Electronic(
+    val id: String,
+    val name: String,
+    val warrantyMonths: Int
+) : Product()
+
+data class Clothing(
+    val id: String,
+    val name: String,
+    val size: String
+) : Product()
