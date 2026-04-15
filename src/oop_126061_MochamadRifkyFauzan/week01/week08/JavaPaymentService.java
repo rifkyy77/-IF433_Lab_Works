a6ea288db17359021732ee29ba554fc074f17f84
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-15 15:36:28 +0700

    week08: (task) implement payment processing with !!

--- src/oop_126061_MochamadRifkyFauzan/week01/week08/JavaPaymentService.java
+++ src/oop_126061_MochamadRifkyFauzan/week01/week08/JavaPaymentService.java
@@ -1,4 +1,8 @@
 package oop_126061_MochamadRifkyFauzan.week01.week08;
 
 public class JavaPaymentService {
+
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
 }
