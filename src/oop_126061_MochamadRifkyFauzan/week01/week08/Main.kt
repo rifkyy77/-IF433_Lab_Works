169a0067a39704c4ab158b5a29cea8c846a39295
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-15 15:02:19 +0700

    week08: implement safe call with let block

--- src/oop_126061_MochamadRifkyFauzan/week01/week08/Main.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week08/Main.kt
@@ -1,2 +1,20 @@
 package oop_126061_MochamadRifkyFauzan.week01.week08
 
+fun main() {
+    println("=== TEST SAFE CALLS & ELVIS ===")
+    val emptyOrder = Order(null, null)
+
+    // rantai safe calls yang elegan
+    val destination =  emptyOrder.deliveryDetails?.address?.city?.name?: "kota tidak diketahui"
+    println("Tujuan pengiriman: $destination")
+
+    println("\n=== TEST LET BLOCK ===")
+    val validOrder = Order(null, 250000)
+
+    val receipt = validOrder.totalPrice?.totalPrice?.let { price ->
+        //blok ini hanya jalan jika totalpriceidak null
+        val tax = price * 0.11
+        "transaksi valid, harga: Rp$price, pajak: Rp$tax"
+    } ?: "transaksi invalid: harga belum di set!"
+    println(receipt)
+}
--- src/oop_126061_MochamadRifkyFauzan/week01/week08/OrderModels.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week08/OrderModels.kt
@@ -1,2 +1,6 @@
 package oop_126061_MochamadRifkyFauzan.week01.week08
 
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
