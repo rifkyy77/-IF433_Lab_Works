c8dfddb2ba53cab2095dd1dc56d9d996805855c6
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-15 15:23:36 +0700

    week08: implement requireNotNull as safer alternative

--- src/oop_126061_MochamadRifkyFauzan/week01/week08/Main.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week08/Main.kt
@@ -49,4 +49,13 @@ fun main() {
     } catch (e: NullPointerException) {
         println("Crash (NPE)! jangan gunakan !! secara sembarangan")
     }
+
+    val apiResponse: Map<String, String?> = mapOf("status" to "200", "token" to null)
+    try{
+        val token = requireNotNull(apiResponse["token"]) {
+            "CRITICAL EXCEPTION: token otentikasi tidak ditemukan dari server!"
+        }
+    } catch (e: IllegalArgumentException) {
+        println(e.message)
+    }
 }
