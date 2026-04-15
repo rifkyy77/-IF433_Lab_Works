b4798d35112e076177d2ccf50bd13a71bd6551c5
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-15 15:26:14 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_126061_MochamadRifkyFauzan.week01.week08;
+
+public class LegacyJavaAPI {
+    // fungsi  ini ditulis dalam java. kotlin tidak tahu apakah kembaliannya nullable
+    public static  String FetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
