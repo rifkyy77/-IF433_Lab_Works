02f2451459600ad1a0c3497671d11ba3427339c1
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-22 14:50:18 +0700

    week09: implement map for key-value storage

--- src/oop_126061_MochamadRifkyFauzan/week01/week09/CollectionsBasics.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week09/CollectionsBasics.kt
@@ -12,4 +12,28 @@ fun main() {
     scores.add(95)
     scores[0] = 88
     println("Mutable List: $scores")
+
+    println("\n=== TEST SET ===")
+
+    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
+    println("Unique Numbers (Set): $uniqueNumbers")
+
+    val activeUsers = mutableSetOf("UserA", "UserB")
+    activeUsers.add("UserC")
+    activeUsers.add("UserA")
+    println("Active Users: $activeUsers")
+
+    println("\n=== TEST MAP ===")
+
+    val studentGrades = mapOf(
+        "Alice" to "A",
+        "Bob" to "B",
+        "Charlie" to "A"
+    )
+    println("Nilai Bob: ${studentGrades["Bob"]}")
+
+    val inventory = mutableMapOf("Apples" to 50, "Bananas" to 30)
+    inventory["Oranges"] = 20
+    inventory["Apples"] = 45
+    println("Inventory: $inventory")
 }
