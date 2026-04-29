0daac1711d578c37e64c40106742b9a4761c9fc9
rifkyy77 <rifkyfauzan82@gmail.com>
2026-04-29 15:07:14 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week10/WalletRepository.kt
@@ -0,0 +1,13 @@
+package oop_126061_MochamadRifkyFauzan.week01.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+
+    fun add(item: T) {
+        items.add(item)
+    }
+
+    fun getAll(): List<T> {
+        return items
+    }
+}
