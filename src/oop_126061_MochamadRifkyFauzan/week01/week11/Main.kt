bdc7dad6819111fd13ef967c77791c1ce491be26
rifkyy77 <rifkyfauzan82@gmail.com>
2026-05-06 14:52:14 +0700

    week11: test extension functions in main

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_126061_MochamadRifkyFauzan.week01.week11
+
+fun main() {
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+}
