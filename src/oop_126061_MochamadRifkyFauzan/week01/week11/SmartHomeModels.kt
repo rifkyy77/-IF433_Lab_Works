5acba1e9d975282990c2585df690fa3cb9657856
rifkyy77 <rifkyfauzan82@gmail.com>
2026-05-06 15:10:11 +0700

    week11: (task) create SmartDevice data class

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week11/SmartHomeModels.kt
@@ -0,0 +1,8 @@
+package oop_126061_MochamadRifkyFauzan.week01.week11
+
+data class SmartDevice(
+    var name: String,
+    var category: String,
+    var isOnline: Boolean = false,
+    var powerLoad: Int = 0
+)
