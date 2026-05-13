93322cbcee5312a906168f229f37c85908ec38bc
rifkyy77 <rifkyfauzan82@gmail.com>
2026-05-13 15:04:03 +0700

    week12: implement basic runCatching

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week12/RunCatchingExample.kt
@@ -0,0 +1,8 @@
+package oop_126061_MochamadRifkyFauzan.week01.week12
+
+fun main() {
+    println("=== TEST RUNCATCHING ===")
+    val result: Result<Int> = runCatching {
+        "42X".toInt()
+    }
+}
