2507b6b319231e59331cd9cf397cfaba188cb9e2
rifkyy77 <rifkyfauzan82@gmail.com>
2026-05-20 23:39:39 +0700

    week13: (task) implement saveTrades using use block and printWriter

--- src/oop_126061_MochamadRifkyFauzan/week01/week13/CryptoTradeLogger.kt
+++ src/oop_126061_MochamadRifkyFauzan/week01/week13/CryptoTradeLogger.kt
@@ -1,5 +1,7 @@
 package oop_126061_MochamadRifkyFauzan.week01.week13
 
+import java.io.File
+
 data class TradeRecord(
     val id: Int,
     val symbol: String,
@@ -24,5 +26,14 @@ fun fromCsvTrade(line: String): TradeRecord? {
     } catch (e: Exception) {
         println("(Log) Data korup diabaikan: $line")
         null
+    }
+}
+
+fun saveTrades(trades: List<TradeRecord>, path: String) {
+    File(path).printWriter().use { writer ->
+
+        trades.forEach {
+            writer.println(it.toCsv())
+        }
     }
 }
