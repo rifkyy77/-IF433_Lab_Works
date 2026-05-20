2dffdbfff2a24fc0c6a3429399ae7b2891c0ff4a
rifkyy77 <rifkyfauzan82@gmail.com>
2026-05-20 23:36:27 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_126061_MochamadRifkyFauzan/week01/week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_126061_MochamadRifkyFauzan.week01.week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
